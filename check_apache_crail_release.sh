ac63f16c551e5119638e06ada79258508db96bf3
Adrian Schuepbach <adrian.schuepbach@gribex.net>
2019-05-28 11:20:32 +0200

    Made Crail version a parameter.

--- check_apache_crail_release.sh
+++ check_apache_crail_release.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+  echo "Please specify a Crail version in this format: apache-crail-1.2-incubating-SNAPSHOT without the .tar.gz suffix.";
+  exit 1;
+fi
+
+crailname=$1
+
 # Make sure to start with a clean tree
-rm -rf apache-crail-1.1-incubating
-rm -rf apache-crail-1.1-incubating-src
+rm -rf $crailname
+rm -rf $crailname-src
 
 
 
 
 
-tar xvzf apache-crail-1.1-incubating-bin.tar.gz
+tar xvzf $crailname-bin.tar.gz
 
 
 mkdir -p /tmp/crail/data
@@ -17,24 +24,24 @@ mkdir -p /tmp/crail/cache
 netiface=`ip -4 -o  address|grep -v 127.0|grep inet|head -1|awk '{print $2}'`
 
 
-echo "crail.namenode.address            crail://localhost:9060" > apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.cachepath                   /tmp/crail/cache" >> apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.cachelimit                  0" >> apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.interface       ${netiface}" >> apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.datapath        /tmp/crail/data" >> apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.storagelimit    1073741824" >> apache-crail-1.1-incubating/conf/crail-site.conf
+echo "crail.namenode.address            crail://localhost:9060" > $crailname/conf/crail-site.conf
+echo "crail.cachepath                   /tmp/crail/cache" >> $crailname/conf/crail-site.conf
+echo "crail.cachelimit                  0" >> $crailname/conf/crail-site.conf
+echo "crail.storage.tcp.interface       ${netiface}" >> $crailname/conf/crail-site.conf
+echo "crail.storage.tcp.datapath        /tmp/crail/data" >> $crailname/conf/crail-site.conf
+echo "crail.storage.tcp.storagelimit    1073741824" >> $crailname/conf/crail-site.conf
 
-cp apache-crail-1.1-incubating/conf/core-site.xml.template apache-crail-1.1-incubating/conf/core-site.xml
+cp $crailname/conf/core-site.xml.template $crailname/conf/core-site.xml
 
-(cd apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail namenode & )
+(cd $crailname && CRAIL_HOME=`pwd` ./bin/crail namenode & )
 sleep 5;
 
-(cd apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail datanode & )
+(cd $crailname && CRAIL_HOME=`pwd` ./bin/crail datanode & )
 sleep 5;
 
 
-(cd apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail fs -touchz /testfile.txt  )
-(cd apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail fs -ls / | grep testfile.txt )
+(cd $crailname && CRAIL_HOME=`pwd` ./bin/crail fs -touchz /testfile.txt  )
+(cd $crailname && CRAIL_HOME=`pwd` ./bin/crail fs -ls / | grep testfile.txt )
 
 if [ $? -ne 0 ]; then
   echo "ERROR: Cannot correctly execute binary release";
@@ -56,9 +63,9 @@ echo ""
 echo ""
 
 
-tar xvzf apache-crail-1.1-incubating-src.tar.gz 
+tar xvzf $crailname-src.tar.gz 
 
-(cd apache-crail-1.1-incubating-src && mvn -DskipTests package)
+(cd $crailname-src && mvn -DskipTests package)
 if [ $? -ne 0 ]; then
   echo "ERROR: Cannot compile the source tarball";
   exit 6;
@@ -68,27 +75,27 @@ rm -rf /tmp/crail/data/*
 rm -rf /tmp/crail/cache/*
 
 
-echo "crail.namenode.address            crail://localhost:9060" > apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.cachepath                   /tmp/crail/cache" >> apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.cachelimit                  0" >> apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.interface       ${netiface}" >> apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.datapath        /tmp/crail/data" >> apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
-echo "crail.storage.tcp.storagelimit    1073741824" >> apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/crail-site.conf
+echo "crail.namenode.address            crail://localhost:9060" > $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
+echo "crail.cachepath                   /tmp/crail/cache" >> $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
+echo "crail.cachelimit                  0" >> $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
+echo "crail.storage.tcp.interface       ${netiface}" >> $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
+echo "crail.storage.tcp.datapath        /tmp/crail/data" >> $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
+echo "crail.storage.tcp.storagelimit    1073741824" >> $crailname-src/assembly/target/$crailname-bin/$crailname/conf/crail-site.conf
 
-cp apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/core-site.xml.template \
-   apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/conf/core-site.xml
+cp $crailname-src/assembly/target/$crailname-bin/$crailname/conf/core-site.xml.template \
+   $crailname-src/assembly/target/$crailname-bin/$crailname/conf/core-site.xml
 
 
 
-(cd apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail namenode & )
+(cd $crailname-src/assembly/target/$crailname-bin/$crailname && CRAIL_HOME=`pwd` ./bin/crail namenode & )
 sleep 5;
 
-(cd apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail datanode & )
+(cd $crailname-src/assembly/target/$crailname-bin/$crailname && CRAIL_HOME=`pwd` ./bin/crail datanode & )
 sleep 5;
 
 
-(cd apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail fs -touchz /testfile.txt  )
-(cd apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating && CRAIL_HOME=`pwd` ./bin/crail fs -ls / | grep testfile.txt )
+(cd $crailname-src/assembly/target/$crailname-bin/$crailname && CRAIL_HOME=`pwd` ./bin/crail fs -touchz /testfile.txt  )
+(cd $crailname-src/assembly/target/$crailname-bin/$crailname && CRAIL_HOME=`pwd` ./bin/crail fs -ls / | grep testfile.txt )
 
 if [ $? -ne 0 ]; then
   echo "ERROR: Cannot correctly execute source release";
@@ -110,30 +117,30 @@ echo ""
 echo ""
 
 # Check signatures and checksums
-sha512sum -c apache-crail-1.1-incubating-bin.tar.gz.sha512
+sha512sum -c $crailname-bin.tar.gz.sha512
 if [ $? -ne 0 ]; then
-  echo "ERROR: CHECKSUM for apache-crail-1.1-incubating-bin.tar.gz WRONG";
+  echo "ERROR: CHECKSUM for $crailname-bin.tar.gz WRONG";
   exit 1;
 fi
 
-sha512sum -c apache-crail-1.1-incubating-src.tar.gz.sha512
+sha512sum -c $crailname-src.tar.gz.sha512
 
 if [ $? -ne 0 ]; then
-  echo "ERROR: CHECKSUM for apache-crail-1.1-incubating-src.tar.gz WRONG";
+  echo "ERROR: CHECKSUM for $crailname-src.tar.gz WRONG";
   exit 2;
 fi
 
-gpg --verify apache-crail-1.1-incubating-bin.tar.gz.asc apache-crail-1.1-incubating-bin.tar.gz
+gpg --verify $crailname-bin.tar.gz.asc $crailname-bin.tar.gz
 
 if [ $? -ne 0 ]; then
-  echo "ERROR: SIGNATURE for apache-crail-1.1-incubating-bin.tar.gz WRONG";
+  echo "ERROR: SIGNATURE for $crailname-bin.tar.gz WRONG";
   exit 3;
 fi
 
-gpg --verify apache-crail-1.1-incubating-src.tar.gz.asc apache-crail-1.1-incubating-src.tar.gz
+gpg --verify $crailname-src.tar.gz.asc $crailname-src.tar.gz
 
 if [ $? -ne 0 ]; then
-  echo "ERROR: SIGNATURE for apache-crail-1.1-incubating-src.tar.gz WRONG";
+  echo "ERROR: SIGNATURE for $crailname-src.tar.gz WRONG";
   exit 4;
 fi
 
@@ -145,13 +152,13 @@ echo ""
 
 
 #Check tarball for licenses, ...
-ls -l apache-crail-1.1-incubating/LICENSE > /dev/null
+ls -l $crailname/LICENSE > /dev/null
 
 if [ $? -ne 0 ]; then
   echo "ERROR: MISSING LICENSE file in binary release";
   exit 11;
 fi
-ls -l apache-crail-1.1-incubating/licenses > /dev/null
+ls -l $crailname/licenses > /dev/null
 
 if [ $? -ne 0 ]; then
   echo "ERROR: MISSING licenses directory in binary release";
@@ -160,11 +167,11 @@ fi
 
 
 lcerrbin=0;
-for f in `ls apache-crail-1.1-incubating/jars/*.jar`; do
+for f in `ls $crailname/jars/*.jar`; do
   b=`basename $f`;
   j=`echo "$b"|grep -v '^crail*'`;
   if [ ! -z  $j  ]; then
-    grep "$j" apache-crail-1.1-incubating/LICENSE > /dev/null;
+    grep "$j" $crailname/LICENSE > /dev/null;
     if [ $? -ne 0 ]; then
       echo "Missing license for $j in binary release";
       lcerrbin=1;
@@ -178,13 +185,13 @@ if [ $lcerrbin -ne 0 ]; then
 fi
 
 
-ls -l apache-crail-1.1-incubating-src/LICENSE > /dev/null
+ls -l $crailname-src/LICENSE > /dev/null
 
 if [ $? -ne 0 ]; then
   echo "ERROR: MISSING LICENSE file in source release";
   exit 11;
 fi
-ls -l apache-crail-1.1-incubating-src/licenses > /dev/null
+ls -l $crailname-src/licenses > /dev/null
 
 if [ $? -ne 0 ]; then
   echo "ERROR: MISSING licenses directory in source release";
@@ -192,11 +199,11 @@ if [ $? -ne 0 ]; then
 fi
 
 lcerrsrc=0;
-for f in `ls apache-crail-1.1-incubating-src/assembly/target/apache-crail-1.1-incubating-bin/apache-crail-1.1-incubating/jars/*.jar`; do
+for f in `ls $crailname-src/assembly/target/$crailname-bin/$crailname/jars/*.jar`; do
   b=`basename $f`;
   j=`echo "$b"|grep -v '^crail*'`;
   if [ ! -z  $j  ]; then
-    grep "$j" apache-crail-1.1-incubating-src/LICENSE-binary > /dev/null;
+    grep "$j" $crailname-src/LICENSE-binary > /dev/null;
     if [ $? -ne 0 ]; then
       echo "Missing license for $j in source release";
       lcerrsrc=1;
@@ -209,8 +216,8 @@ done
 
 
 
-rm -rf apache-crail-1.1-incubating
-rm -rf apache-crail-1.1-incubating-src
+rm -rf $crailname
+rm -rf $crailname-src
 rm -rf /tmp/crail/data
 rm -rf /tmp/crail/cache
 
